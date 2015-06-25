3af62822d360a0c339f631613b1bb890715adfb6
Viktor Somodi <somodi.viktor@gmail.com>
2015-06-25 15:03:33 +0200

    es5ify

--- index.js
+++ index.js
@@ -4,56 +4,56 @@ var EventEmitter = require('events').EventEmitter;
 
 module.exports = function(angular) {
 
-  return class App {
+  var App = function(angularApp, config) {
+    this._name = config.name;
+    this._cssUrl = config.cssUrl;
+    this._angularApp = angularApp;
+    this._eventEmitter = new EventEmitter();
+    this._api = null;
 
-    constructor (angularApp, config) {
-      this._name = config.name;
-      this._cssUrl = config.cssUrl;
-      this._angularApp = angularApp;
-      this._eventEmitter = new EventEmitter();
-      this._api = null;
-
-      this._angularApp.on('started', (api) => {
-        this._api = api;
-        this._eventEmitter.emit('initialized', api);
-      });
-    }
+    this._angularApp.on('started', function(api) {
+      this._api = api;
+      this._eventEmitter.emit('initialized', api);
+    }.bind(this));
+  };
 
 
-    runAngularApp() {
+  App.prototype = {
+
+    runAngularApp: function() {
       this._angularApp.registerModules();
       this._inject();
       this._bootstrap();
-    }
+    },
 
 
-    _inject() {
+    _inject: function() {
       this._injectHtml(`<app id="${this._name}"></app>`);
       this._injectCss(this._cssUrl);
-    }
+    },
 
 
-    _bootstrap() {
+    _bootstrap: function() {
       angular.bootstrap(document.getElementById(this._name), [this._name]);
-    }
+    },
 
 
-    getApi() {
+    getApi: function() {
       return this._api;
-    }
+    },
 
 
-    on(name, cb) {
+    on: function(name, cb) {
       this._eventEmitter.on(name, cb);
-    }
+    },
 
 
-    _injectHtml(content) {
+    _injectHtml: function(content) {
       angular.element(document.body).append(content);
-    }
+    },
 
 
-    _injectCss(path) {
+    _injectCss: function(path) {
       var head = document.getElementsByTagName('head')[0];
       var link = document.createElement('link');
       link.setAttribute('href', path);
@@ -62,6 +62,8 @@ module.exports = function(angular) {
       head.appendChild(link);
     }
 
-  }
+  };
+
+  return App;
 
 };
